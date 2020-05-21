99f9a5d7850d315d6d77f3f9dd1654a7cbb6f117
dmytro shabanov <dmytro.shabanov98@gmail.com>
2020-05-21 16:31:45 +0300

    add remote ok parser solution

---
+++ src/remoteok/RemoteOk/RemoteOk/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace RemoteOk
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            IWebDriver Driver;
+
+            using (Driver = new ChromeDriver())
+            {
+                Driver.Url = "https://remoteok.io/remote-dev-jobs";
+            }
+        }
+    }
+}
